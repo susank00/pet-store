e4aaf2783774a39c30ef020e882748c5f5e51382
susankhatri00 <susankhatri00@gmail.com>
2024-02-01 00:27:03 +0545

    functionality of admin to view all employees name is completed successfully

--- client/src/assets/Profile.jsx
+++ client/src/assets/Profile.jsx
@@ -9,49 +9,48 @@ const Profile = () => {
   const [role, setRole] = useState("");
   const userEmail = location.state && location.state.email;
   const [employeeNames, setEmployeeNames] = useState([]);
+
+  const fetchUserInfo = async () => {
+    try {
+      const response = await axios.post("http://localhost:3001/getUserInfo", {
+        email: userEmail,
+      });
+
+      if (response.data.success) {
+        setName(response.data.name);
+        setPassword(response.data.password);
+        setRole(response.data.role);
+      }
+      if (response.data.role === "admin") {
+        fetchEmployeeNames();
+      } else {
+        console.error("User information not found:", response.data.message);
+      }
+    } catch (error) {
+      console.error("Error fetching user information:", error.message);
+    }
+  };
+
   useEffect(() => {
-    const fetchEmployeeNames = async () => {
-      try {
-        const response = await fetch("http://localhost:3001/employeeNames");
-        const namesData = await response.json();
-        setEmployeeNames(namesData);
-      } catch (error) {
-        console.error("Error fetching employee names:", error);
-      }
-    };
-
-    const fetchUserInfo = async () => {
-      try {
-        const response = await axios.post("http://localhost:3001/getUserInfo", {
-          email: userEmail,
-        });
-
-        if (response.data.success) {
-          setName(response.data.name);
-          setPassword(response.data.password);
-          setRole(response.data.role);
-        } else {
-          console.error("User information not found:", response.data.message);
-        }
-      } catch (error) {
-        console.error("Error fetching user information:", error.message);
-      }
-    };
-
     if (userEmail) {
       fetchUserInfo();
     }
-
-    fetchEmployeeNames(); // Move this line outside of fetchUserInfo if you want it to run unconditionally
   }, [userEmail]);
+
+  const fetchEmployeeNames = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/employeeNames");
+      setEmployeeNames(response.data);
+    } catch (error) {
+      console.error("Error fetching employee names:", error);
+    }
+  };
 
   return (
     <>
       {name || password ? (
         <div>
-          Welcome,
-          {name}
-          (Role: {role})<h1>List of Employee Names</h1>
+          Welcome, {name} (Role: {role})<h1>List of Employee Names</h1>
           <ul>
             {employeeNames.map((employeeName, index) => (
               <li key={index}>{employeeName}</li>
