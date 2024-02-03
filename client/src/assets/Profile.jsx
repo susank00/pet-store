01501518a479b0ec646545eba76b68051c17ccbf
susankhatri00 <susankhatri00@gmail.com>
2024-02-03 12:49:34 +0545

    added login logout functionality dynamic

--- client/src/assets/Profile.jsx
+++ client/src/assets/Profile.jsx
@@ -1,22 +1,16 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import MyNavbar from "../components/MyNavbar";
-
-import "flowbite";
-
+import Loginhandler from "../functionalcomponent/Loginhandler";
 const Profile = () => {
   const location = useLocation();
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
   const userEmail = location.state && location.state.email;
-
-  useEffect(() => {
-    getProfile();
-    // Fetch user info only once when the component mounts
-    fetchUserInfo();
-  }, []);
+  const [isloggedin, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
   const getProfile = async () => {
     const getAccessToken = localStorage.getItem("accessToken");
@@ -54,15 +48,50 @@ const Profile = () => {
       console.error("Error fetching user information:", error.message);
     }
   };
+  const checkUserStatus = async () => {
+    const getAccessToken = localStorage.getItem("accessToken");
+    if (getAccessToken) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  };
+  const onLogout = async () => {
+    localStorage.removeItem("accessToken");
+    setIsLoggedIn(false); //
+    navigate("/");
+  };
+  const onLogin = async () => {
+    navigate("/login");
+  };
+  useEffect(() => {
+    // Fetch user info only once when the component mounts
+    fetchUserInfo();
+    getProfile();
+    checkUserStatus();
+  }, []);
 
   return (
     <>
       <MyNavbar />
-
+      <Loginhandler />
       {name || password ? (
         <div>
           Welcome, {name} <br />
           (Role: {role})<br />
+          <div>
+            {isloggedin ? (
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                onClick={onLogout}
+              >
+                Logout
+              </button>
+            ) : (
+              <button onClick={onLogin}>Login</button>
+            )}
+          </div>
           {role === "admin" && (
             <>
               {/* Render Adminfunction only if the role is admin */}{" "}
---
+++ client/src/functionalcomponent/Loginhandler.jsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Loginhandler = () => {
+  const [isloggedin, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  const getProfile = async () => {
+    const getAccessToken = localStorage.getItem("accessToken");
+    try {
+      const response = await axios.get("http://localhost:3001/profile", {
+        timeout: 10000,
+        headers: {
+          Authorization: `Bearer ${getAccessToken}`,
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.errors[0].message);
+      } else {
+        alert("unkknown error,please try again");
+      }
+    }
+  };
+  const checkUserStatus = async () => {
+    const getAccessToken = localStorage.getItem("accessToken");
+    if (getAccessToken) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  };
+  const onLogout = async () => {
+    localStorage.removeItem("accessToken");
+    setIsLoggedIn(false); //
+    navigate("/");
+  };
+  const onLogin = async () => {
+    navigate("/login");
+  };
+  useEffect(() => {
+    getProfile();
+    checkUserStatus();
+  }, []);
+
+  return (
+    <>
+      <div>
+        {isloggedin ? (
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            onClick={onLogout}
+          >
+            Logout
+          </button>
+        ) : (
+          <button onClick={onLogin}>Login</button>
+        )}
+      </div>
+    </>
+  );
+};
+export default Loginhandler;
