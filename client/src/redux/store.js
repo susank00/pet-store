d44412cc68d020b3b0ecd98f8bbecba72fd395ce
susankhatri00 <susankhatri00@gmail.com>
2024-02-07 12:48:02 +0545

    some fixes in buy page

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,15 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
-import userReducer from "./reducers/"; // Import your user reducer here
+import userReducer from "./reducers"; // Import your user reducer here
 
 // Combine reducers if you have multiple reducers
 const rootReducer = combineReducers({
-  user: userReducer, // Add more reducers here if needed
+  userReducer, // Add more reducers here if needed
 });
 
 // Create the Redux store with the combined reducers
+
 const store = configureStore({
-  reducer: rootReducer, // Pass the combined reducers object
+  reducer: rootReducer,
+  // Optionally, you can provide middleware, devtools configuration, etc.
 });
 
 export default store;
