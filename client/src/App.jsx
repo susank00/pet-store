b17576a6ee7d6bc5100b1117c5158da8a729377c
susankhatri00 <susankhatri00@gmail.com>
2024-09-17 08:37:04 +0545

    configuration changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ import Home from "./pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Adminfunction from "./adminfucntions//Adminfunction";
 import UserFunction from "./userfunctions/UserFunction";
-import Buy from "./pages/buy";
 import AddProductForm from "./pages/AddProductForm";
 import Products from "./pages/Products";
 import MyNavbar from "./components/MyNavbar";
@@ -28,7 +27,6 @@ const App = () => {
           <Route path="/register" element={<Signup />} />
           <Route path="/admin" element={<Adminfunction />} />
           <Route path="/User" element={<UserFunction />} />
-          <Route path="/buy" element={<Buy />} />
           <Route path="/addproduct" element={<AddProductForm />} />
           <Route path="/products" element={<Products />} />
           <Route path="/sidenav" element={<SideNavbar />} />
