ddfa518a7fe9fa6f0779b69c06e0ba48010ac1b1
susankhatri00 <susankhatri00@gmail.com>
2024-09-18 22:40:18 +0545

    some changes in index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use("/public", express.static("public"));
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
