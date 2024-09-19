34dfab8aff8a22b6d89b1f470b70f1bd9263a1e8
susankhatri00 <susankhatri00@gmail.com>
2024-09-19 17:54:54 +0545

    all set

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 // app.use(express.static("public"));
-app.use("/public", express.static("public"));
+app.use("/public", express.static(path.join(__dirname, "public")));
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
