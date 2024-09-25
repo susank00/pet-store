d51aae5c29675da8ff5e46ca6fffefe56654a030
susankhatri00 <susankhatri00@gmail.com>
2024-09-26 00:31:25 +0545

    added bcrypt in password hashing and fix broken authentication in admin menu and adminfunction

--- client/src/adminfucntions/Adminfunction.jsx
+++ client/src/adminfucntions/Adminfunction.jsx
@@ -5,45 +5,63 @@ import { useDispatch } from "react-redux";
 import { setSelectedEmployeesId } from "../redux/actions";
 import AdminUserEdit from "./Adminuseredit";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode"; // Ensure this import is correct
 
 const Adminfunction = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [employees, setEmployees] = useState([]);
   const [showAdminUserEdit, setShowAdminUserEdit] = useState(false);
+  const [rolee, setRole] = useState(null); // State to hold user role
+
+  // Fetch employee names
+  const fetchEmployees = async (accessToken) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL_PROD_API_URL}/employeeNames`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+          timeout: 1000,
+        }
+      );
+      setEmployees(response.data);
+    } catch (error) {
+      console.error("Error fetching employee names:", error);
+      if (error.response && error.response.status === 401) {
+        navigate("/login"); // Redirect to login if unauthorized
+      }
+    }
+  };
 
   // Check for token and fetch employees
   useEffect(() => {
     const getAccessToken = localStorage.getItem("accessToken");
 
     if (!getAccessToken) {
-      navigate("/login"); // Redirect to login if token is not found
+      navigate("/login");
       return;
     }
 
-    const fetchEmployees = async () => {
-      try {
-        const response = await axios.get(
-          `${import.meta.env.VITE_API_URL_PROD_API_URL}/employeeNames`,
-          {
-            headers: {
-              Authorization: `Bearer ${getAccessToken}`,
-            },
-            timeout: 1000,
-          }
-        );
-        setEmployees(response.data);
-      } catch (error) {
-        console.error("Error fetching employee names:", error);
-        // Handle error (e.g., redirect to login if token is invalid)
-        if (error.response && error.response.status === 401) {
-          navigate("/login"); // Redirect to login if unauthorized
-        }
-      }
-    };
+    try {
+      const decodedToken = jwtDecode(getAccessToken);
+      setRole(decodedToken.role); // Save user role
 
-    fetchEmployees();
-  }, [navigate, showAdminUserEdit]); // Removed 'employees' from dependency array
+      // Now fetch employees after decoding the token
+      fetchEmployees(getAccessToken);
+    } catch (error) {
+      console.error("Failed to decode access token:", error);
+      navigate("/login");
+    }
+  }, [navigate]);
+
+  // Redirect if role is not admin
+  useEffect(() => {
+    if (rolee !== null && rolee !== "admin") {
+      navigate("/profile"); // Redirect if role is not admin
+    }
+  }, [rolee, navigate]); // Add rolee and navigate to dependencies
 
   // Handle edit click
   const handleEditClick = (employeeId) => {
@@ -55,59 +73,61 @@ const Adminfunction = () => {
   return (
     <>
       <SideNavbar />
-      <div className="ml-64 relative overflow-x-auto">
-        <h1 className="bg-gray-600 text-5xl text-red-600 text-center p-2">
-          All Employees
-        </h1>
-        {showAdminUserEdit && <AdminUserEdit />}
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Employee Name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Employee ID
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Role
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Email
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Modify
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {employees.map((employee) => (
-              <tr
-                key={employee._id}
-                className="bg-red-900 border-b dark:bg-gray-800 dark:border-gray-700"
-              >
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {employee.name}
+      {rolee === "admin" && (
+        <div className="ml-64 relative overflow-x-auto">
+          <h1 className="bg-gray-600 text-5xl text-red-600 text-center p-2">
+            All Employees
+          </h1>
+          {showAdminUserEdit && <AdminUserEdit />}
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Employee Name
                 </th>
-                <td className="px-6 py-4">{employee._id}</td>
-                <td className="px-6 py-4">{employee.role}</td>
-                <td className="px-6 py-4">{employee.email}</td>
-                <td className="px-6 py-4">
-                  <button
-                    onClick={() => handleEditClick(employee._id)}
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    Edit
-                  </button>
-                </td>
+                <th scope="col" className="px-6 py-3">
+                  Employee ID
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Role
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Email
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Modify
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {employees.map((employee) => (
+                <tr
+                  key={employee._id}
+                  className="bg-red-900 border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {employee.name}
+                  </th>
+                  <td className="px-6 py-4">{employee._id}</td>
+                  <td className="px-6 py-4">{employee.role}</td>
+                  <td className="px-6 py-4">{employee.email}</td>
+                  <td className="px-6 py-4">
+                    <button
+                      onClick={() => handleEditClick(employee._id)}
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Edit
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </>
   );
 };
--- client/src/adminfucntions/Adminmenu.jsx
+++ client/src/adminfucntions/Adminmenu.jsx
@@ -2,14 +2,39 @@ import { useEffect, useState } from "react";
 import SideNavbar from "../components/SideNavbar";
 import axios from "axios";
 import AddProductForm from "../pages/AddProductForm";
+import { jwtDecode } from "jwt-decode"; // Correct import
+import { useNavigate } from "react-router-dom"; // For navigation
 
 const Adminproductlist = () => {
   const [products, setProducts] = useState([]);
   const [showAddProductForm, setShowAddProductForm] = useState(false);
+  const [role, setRole] = useState();
+  const navigate = useNavigate();
 
   const handleAddProductClick = () => {
     setShowAddProductForm(true);
   };
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        setRole(decodedToken.role);
+      } catch (error) {
+        console.error("Failed to decode token", error);
+        navigate("/login"); // Redirect if the token is invalid
+      }
+    } else {
+      navigate("/login"); // Redirect if token is missing
+    }
+  }, [navigate]);
+
+  useEffect(() => {
+    if (role && role !== "admin") {
+      navigate("/profile"); // Redirect if role is not admin
+    }
+  }, [role, navigate]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -29,69 +54,71 @@ const Adminproductlist = () => {
   return (
     <>
       <SideNavbar />
-      <div className="ml-64 relative overflow-x-auto">
-        <h1 className="bg-gray-600 text-5xl text-red-600 text-center p-2">
-          All Products
-        </h1>
+      {role === "admin" && (
+        <div className="ml-64 relative overflow-x-auto">
+          <h1 className="bg-gray-600 text-5xl text-red-600 text-center p-2">
+            All Products
+          </h1>
 
-        <div className="p-1 flex justify-center bg-gray-600">
-          <button
-            type="button"
-            onClick={handleAddProductClick}
-            className=" text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            + Add Product
-          </button>
-        </div>
-        {showAddProductForm && <AddProductForm />}
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Description
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                modify
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr
-                key={product._id}
-                className="bg-red-900 border-b dark:bg-gray-800 dark:border-gray-700"
-              >
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {product.name}
+          <div className="p-1 flex justify-center bg-gray-600">
+            <button
+              type="button"
+              onClick={handleAddProductClick}
+              className=" text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              + Add Product
+            </button>
+          </div>
+          {showAddProductForm && <AddProductForm />}
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Product name
                 </th>
-                <td className="px-6 py-4">{product.category}</td>
-                <td className="px-6 py-4">{product.description}</td>
-                <td className="px-6 py-4">${product.price}</td>
-                <td className="px-6 py-4 ">
-                  <a
-                    href=""
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    Edit
-                  </a>
-                </td>
+                <th scope="col" className="px-6 py-3">
+                  Category
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Description
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Modify
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr
+                  key={product._id}
+                  className="bg-red-900 border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {product.name}
+                  </th>
+                  <td className="px-6 py-4">{product.category}</td>
+                  <td className="px-6 py-4">{product.description}</td>
+                  <td className="px-6 py-4">${product.price}</td>
+                  <td className="px-6 py-4">
+                    <a
+                      href=""
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Edit
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </>
   );
 };
